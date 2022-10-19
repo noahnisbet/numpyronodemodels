e097b338e205980b9ebb00422aade818ce8a86d5
noah-gettogetherai <noahnisbet3@gmail.com>
2022-10-19 11:02:31 -0400

    Adding basic setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='numpyronodemodels',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Noah Nisbet',
+    author_email='nnisbet@clemson.edu',
+    description='This package is build on NumPyro and helps with the creation of large inference models. '
+)
